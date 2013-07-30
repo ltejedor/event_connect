119a82fa241c745821c72400f0809634316d2e75
Leandra Tejedor <lmtejedor@gmail.com>
2013-07-30 09:23:35 -0700

    Add title functionality for show page

--- spec/integration/creating_events_spec.rb
+++ spec/integration/creating_events_spec.rb
@@ -7,5 +7,9 @@ feature 'Creating Events' do
     fill_in 'Title', :with => 'Shoot a commercial'
     click_button 'Create Event'
     page.should have_content('Event has been created.')
+    event = Event.find_by_title("Shoot a commercial")
+    page.current_url.should == event_url(event)
+    title = "Shoot a commercial - Events - EventConnect"
+    find("title").should have_content(title)
   end
 end
